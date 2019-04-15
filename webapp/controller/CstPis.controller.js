3e69871a8f20bd2660a96eb8eea898e5fa9085a3
matusca96 <gomesloud@gmail.com>
2019-04-15 16:06:00 -0300

    Padronização.

--- webapp/controller/CstPis.controller.js
+++ webapp/controller/CstPis.controller.js
@@ -1,12 +1,25 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageBox"
-], function(Controller, MessageBox) {
+	"sap/m/MessageBox",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function(Controller, MessageBox, Filter, FilterOperator) {
 	"use strict";
 
 	return Controller.extend("br.com.idxtecCstPis.controller.CstPis", {
 		onInit: function(){
 			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		},
+		
+		filtraPis: function(oEvent){
+			var sQuery = oEvent.getParameter("query");
+			var oFilter = new Filter("Codigo", FilterOperator.Contains, sQuery);
+			
+			var aFilters = [
+				oFilter
+			];
+
+			this.getView().byId("tableCstPis").getBinding("rows").filter(aFilters, "Application");
 		},
 		
 		onRefresh: function(){
